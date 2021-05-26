16370906e0a749621d54e735c369af7166dd7fa7
José Ignacio Lucas Lledó <ignasilucas@gmail.com>
2021-05-26 22:59:33 +0200

    Preparant alguns exercicis.

---
+++ preparar_ambiente.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BLAST_VERSION=0
+if $(which blastp > /dev/null); then
+   BLAST_VERSION=$(blastp -version | head -n 1 | cut -d " " -f 2)
+fi
+
+if [ $BLAST_VERSION != "2.10.1+" ]; then
+   conda install -c bioconda -y -q blast=2.10.1
+fi
+
+if [ ! -e taxdb.btd ]; then
+   if [ -e taxdb.btd.gz ]; then
+      gunzip taxdb.btd.gz
+   else
+      echo "Falta l'arxiu taxdb.btd.gz!"
+   fi
+fi
